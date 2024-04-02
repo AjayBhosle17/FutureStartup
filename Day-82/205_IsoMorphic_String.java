de47befe3fe5694da80e6e6099020cae0bf74cd6
Ajay Bhosle <ajaybhosle8805@gmail.com>
2024-04-02 11:58:24 +0530

    205_IsomorphicString

---
+++ Day-82/205_IsoMorphic_String.java
@@ -0,0 +1,42 @@
+
+import java.util.*;
+class Solution {
+
+	
+    	static boolean isIsomorphic(String s, String t) {
+       
+
+		HashMap <Character,Character>hm= new HashMap<>();
+		
+		if(s.length()!=t.length()){
+		
+			return false;
+		}
+		for(int i=0 ;i<s.length();i++){
+		
+			if(hm.containsKey(s.charAt(i))){
+			
+				if(!hm.get(s.charAt(i)).equals(t.charAt(i))){
+				
+					return false;
+				}
+			}else{
+			
+				if(hm.containsValue(t.charAt(i))){
+				
+					return false;
+				}
+
+				hm.put(s.charAt(i),t.charAt(i));
+			}
+
+		}
+		return true;
+   
+       	}
+
+	public static void main(String [] args){
+	
+		System.out.println(isIsomorphic("abb","ede"));
+	}
+}
