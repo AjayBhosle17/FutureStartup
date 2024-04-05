0b61e558d37160c54c4c7d82afcfa335190e9f46
Ajay Bhosle <ajaybhosle8805@gmail.com>
2024-04-05 11:49:53 +0530

    81_Rotated_SearchII

---
+++ Day-84/81_Rotated_SearchII.java
@@ -0,0 +1,43 @@
+class Ajay {
+    static boolean search(int[] nums, int target) {
+        
+        if(nums.length <2 && target == nums[0]) return true; 
+        if(nums.length <2 && target != nums[0]) return false; 
+        int low = 0;
+        int high = nums.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target || nums[low]==target || nums[high]==target){
+                return true;
+            }else if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low++;
+                high--;
+                continue;
+            }
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target <= nums[mid]){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }else{
+                if(nums[mid]<= target && target <= nums[high]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String [] args){
+    
+	    int arr[]={0,1,2,3,5};
+
+	    int target=2;
+
+	    System.out.println(search(arr,target));
+    }
+}
