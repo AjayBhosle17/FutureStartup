f332c9e9c3302ee1ef82de0ba3adfaba86c86e00
Ajay Bhosle <ajaybhosle8805@gmail.com>
2024-04-12 06:12:11 +0530

    231_PowerOf_Two

---
+++ Day-91/231_Power_Of_Two.java
@@ -0,0 +1,21 @@
+class Ajay{
+
+	static boolean isPowerOfTwo(int n) {
+
+		if(n==0){
+		
+			return false;
+		}
+
+		while(n%2==0){
+		
+			n/=2;
+		}
+		return n==1;
+	}
+
+	public static void main(String [] args){
+	
+		System.out.println(isPowerOfTwo(16));
+	}
+}
