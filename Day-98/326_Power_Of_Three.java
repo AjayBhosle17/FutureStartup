2f9fb25361da512c2fcc53fbf267663faa03b46e
Ajay Bhosle <ajaybhosle8805@gmail.com>
2024-04-18 20:21:09 +0530

    326_PowerOf_Three

---
+++ Day-98/326_Power_Of_Three.java
@@ -0,0 +1,26 @@
+class Ajay {
+
+	static boolean isPower(int n){
+	
+		if(n<=0){
+		
+			return false;
+		}
+
+		while(n>1){
+		
+			if(n%3!=0){
+			
+				return false;
+			}
+			n/=3;
+		}
+		return true;
+	}
+
+	public static void main(String [] args){
+	
+		System.out.println(isPower(6));
+
+	}
+}
